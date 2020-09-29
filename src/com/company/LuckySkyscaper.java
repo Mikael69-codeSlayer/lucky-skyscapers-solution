cd0212e1f072d041e4b01d71d56f1d2cc9afce68
Mikael <mikael.sardor@studerande.plushogskolan.se>
2020-09-29 11:32:43 +0200

    The real Floor method works

--- src/com/company/LuckySkyscaper.java
+++ src/com/company/LuckySkyscaper.java
@@ -2,18 +2,19 @@ package com.company;
 
 public class LuckySkyscaper {
 
-    static public void floorList(int totalRealFloors){
+    static public int toRealFloor(int fakeFloorGoal){
         // loop through real floor numbers
         var fakeFloor = 1;
-        for(var realFloor = 1; realFloor <= totalRealFloors; realFloor++) {
-            System.out.println("realFloorNo: " + realFloor + " fakeFloorNo: " + fakeFloor);
-            // add 1 to fakeFLoor and continue adding 1 as long as
-            // fakeFloor ends with 4 or ends with 13
+        for(var realFloor = 1; realFloor <= fakeFloorGoal; realFloor++) {
+            if(fakeFloorGoal == fakeFloor) {
+                return realFloor; // this ends the loop and the method
+            }
            do {
                fakeFloor++;
            } while(fakeFloor % 10 == 4 || fakeFloor % 100 == 13);
 
 
         }
+        return -1; // could not find real floor number
     }
 }
