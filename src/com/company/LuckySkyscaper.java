6596627891833bfdf90551ab80e535a024f2a201
Mikael <mikael.sardor@studerande.plushogskolan.se>
2020-09-29 10:58:14 +0200

    Looping through real floor numbers

--- src/com/company/LuckySkyscaper.java
+++ src/com/company/LuckySkyscaper.java
@@ -2,4 +2,10 @@ package com.company;
 
 public class LuckySkyscaper {
 
+    static public void floorList(int totalRealFloors){
+        // loop through real floor numbers
+        for(var realFloor = 1; realFloor <= totalRealFloors; realFloor++) {
+            System.out.println("realFloorNo: " + realFloor);
+        }
+    }
 }
