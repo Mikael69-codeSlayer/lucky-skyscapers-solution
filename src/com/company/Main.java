a9bcb38432ad813643799b8903f767bc7a7ee306
Mikael <mikael.sardor@studerande.plushogskolan.se>
2020-09-29 13:51:35 +0200

    Testing memoized version

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,12 +3,26 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-      for(var floor = 1; floor <= 220; floor++) {
+
+        // Test to non-memoized version
+     /* for(var floor = 1; floor <= 220; floor++) {
           System.out.println("floor: " + floor);
           var fakeFloor = LuckySkyscaper.toFakeFloor(floor);
           System.out.println("fakeFloor: " + LuckySkyscaper.toRealFloor(floor));
           var backToRealFloor = LuckySkyscaper.toRealFloor(fakeFloor);
           System.out.println("  back to real: " + backToRealFloor);
-      }
+      } */
+
+        // Test memoized version
+
+        var skyScaper = new LuckySkyScaperMemoized(220);
+
+        for(var floor = 1; floor <= 220; floor++) {
+            System.out.println("floor: " + floor);
+            var fakeFloor = skyScaper.toFakeFloor(floor);
+            System.out.println("fakeFloor: " + LuckySkyscaper.toRealFloor(floor));
+            var backToRealFloor = skyScaper.toRealFloor(fakeFloor);
+            System.out.println("  back to real: " + backToRealFloor);
+        }
     }
 }
